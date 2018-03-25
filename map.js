b78ec2f921692fee0810a58dbe9f10b5e5e16863
kwant <kwant.matthieu@gmail.com>
2018-03-26 00:37:41 +0200

    ajout du fichier contenant les API keys dans le gitignore pour plus de sécurité

--- map.js
+++ map.js
@@ -1,9 +1,16 @@
-console.log("yo");
+var map;
+function initMap() {
+  // Instancie une nouvelle carte à partir de la classe MAP.
+  // Le premier paramètre est le noeud du dom qui contiendra la carte,
+  // le deuxième est le constructeur
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 48.7833, lng: 2.4667},
+    zoom: 13
+  });
+}
 
-  var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 48.7833, lng: 2.4667},
-      zoom: 12
-    });
-  }
+ajaxGet(JCDecauxKey, function(reponse) {
+ // Récupère les data JSON de l'API et les transforme en un tableau JavaScript
+  var bike = JSON.parse(reponse);
+  console.log(bike);
+});
